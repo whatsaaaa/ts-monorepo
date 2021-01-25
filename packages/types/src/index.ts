c697874ce78deaf165a1cc6817296eaf2ec1649e
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-01-25 15:12:52 +0100

    feat(types): add iuser interface

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -14,4 +14,4 @@
  * @packageDocumentation
  */
 export { isTypedArray, isMessage } from "./type-guards";
-export { IMessage, ITeam, IChannel } from "./types";
+export { IMessage, ITeam, IChannel, IUser } from "./types";
