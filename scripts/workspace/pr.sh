bf2bebf7291c673a96318d396b2f31966c5ae224
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-01-25 14:19:58 +0100

    chore: simplify pr.sh

--- scripts/workspace/pr.sh
+++ scripts/workspace/pr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "┏━━━ 📦 Push Workspace ━━━━━━━━━━━━━━━━━━━"
-yarn clean && yarn build && yarn api-docs && lerna run api-report && git add -A && git commit -m "chore: docs"
+echo "┏━━━ 📦 Generating docs for Pull Request ━━━━━━━━━━━━━━━━━━━"
+yarn api-docs && git add -A && git commit -m "chore: docs"
