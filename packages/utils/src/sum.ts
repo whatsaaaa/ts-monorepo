76c96b23a13826cb89a2d3ddf04d1c3487909244
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2021-01-25 11:29:31 +0100

    feat(utils): sum2 in beta

--- packages/utils/src/sum.ts
+++ packages/utils/src/sum.ts
@@ -8,3 +8,14 @@
 export function sum(a: number, b: number): number {
   return a + b;
 }
+
+/**
+ * Calculates the sum of two numbers
+ * @param a - first number
+ * @param b - second number
+ *
+ * @beta
+ */
+export function sum2(a: number, b: number): number {
+  return a + b;
+}
